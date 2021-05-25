5d0a268a22fb34f679f453741a4f33a0ba3bbb8b
Tobias Rapp <yahuxo+github@mailbox.org>
2021-05-25 21:29:33 +0200

    Avoid logging by default when compiled as a native library
    Only write log output when explicitly requested for targets besides
    WASM. Avoids too many messages on standard error when used as a native
    library and in examples.

--- src/logger.rs
+++ src/logger.rs
@@ -43,62 +43,58 @@ macro_rules! log {
 }
 
 //----------------------------------------------------------------------------
-// The following log macros are active only when compiled in debug mode
+// The following log macros are active only when compiled with the "logger" feature
 
 /// Prints an info message to the log output.
-#[cfg(debug_assertions)]
+///
+/// Log output is skipped when not compiled with the "logger" feature.
 #[cfg(target_arch = "wasm32")]
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)+) => {
-        web_sys::console::info_1(&format!($($arg)*).into());
+        if cfg!(feature = "logger") {
+            web_sys::console::info_1(&format!($($arg)*).into());
+        }
     }
 }
 
 /// Prints an info message to the log output.
-#[cfg(debug_assertions)]
+///
+/// Log output is skipped when not compiled with the "logger" feature.
 #[cfg(not(target_arch = "wasm32"))]
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)+) => {
-        eprintln!($($arg)*);
+        if cfg!(feature = "logger") {
+            eprintln!($($arg)*);
+        }
     }
 }
 
-#[cfg(not(debug_assertions))]
-#[macro_export]
-macro_rules! info {
-    ($($arg:tt)+) => {
-        let _ = ($($arg)*);
-    };
-}
-
 /// Prints a debug message to the log output.
-#[cfg(debug_assertions)]
+///
+/// Log output is skipped when not compiled with the "logger" feature.
 #[cfg(target_arch = "wasm32")]
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)+) => {
-        web_sys::console::debug_1(&format!($($arg)*).into());
+        if cfg!(feature = "logger") {
+            web_sys::console::debug_1(&format!($($arg)*).into());
+        }
     }
 }
 
 /// Prints a debug message to the log output.
-#[cfg(debug_assertions)]
+///
+/// Log output is skipped when not compiled with the "logger" feature.
 #[cfg(not(target_arch = "wasm32"))]
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)+) => {
-        eprintln!($($arg)*);
+        if cfg!(feature = "logger") {
+            eprintln!($($arg)*);
+        }
     }
-}
-
-#[cfg(not(debug_assertions))]
-#[macro_export]
-macro_rules! debug {
-    ($($arg:tt)+) => {
-        let _ = ($($arg)*);
-    };
 }
 
 //----------------------------------------------------------------------------
