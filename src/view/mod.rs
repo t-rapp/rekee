8c9fe528113b75e715859c5cba5ef0414e2bd616
Tobias Rapp <yahuxo+github@mailbox.org>
2021-06-10 12:52:28 +0200

    Add attributes for high-quality tile image scaling
    The SVG "image-rendering" attribute improves display of tile images in
    some browsers (Firefox), use the CSS "image-rendering" attribute for a
    similar effect in other browsers (Chrome, Edge).

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -58,6 +58,7 @@ fn draw_tile<C, D>(document: &Document, layout: &Layout, id: TileId, pos: C, dir
     img.set_attribute("href", &format!("tiles/thumb-{}.png", id))?;
     img.set_attribute("width", &format!("{}", 2.0 * size.x()))?;
     img.set_attribute("height", &format!("{}", 2.0 * size.y()))?;
+    img.set_attribute("image-rendering", "optimizeQuality")?;
     img.set_attribute("transform", &format!("rotate({:.0}) translate({:.3} {:.3})", angle, -size.x(), -size.y()))?;
 
     let label = document.create_element_ns(SVG_NS, "text")?;
