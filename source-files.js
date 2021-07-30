2855128079d69e67b2ed3331eb3077bb14958054
t-rapp <t-rapp@users.noreply.github.com>
2021-07-30 05:44:56 +0000

    deploy: 631a5bd82988a39bc91580a3c190e65c737ae0b1

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rekee"] = {"name":"","dirs":[{"name":"view","files":["catalog.rs","catalog_config.rs","export.rs","map.rs","mod.rs","version.rs","welcome.rs"]}],"files":["controller.rs","edition.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs"]};
+sourcesIndex["rekee"] = {"name":"","dirs":[{"name":"view","files":["catalog.rs","catalog_config.rs","export.rs","map.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]}],"files":["controller.rs","edition.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs"]};
 createSourceSidebar();
