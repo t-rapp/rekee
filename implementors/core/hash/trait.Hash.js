9e12b2e517b25e2076a1d6fad6f45c0d99d994cd
t-rapp <t-rapp@users.noreply.github.com>
2021-09-17 14:36:11 +0000

    deploy: dbfe0bde8424ff8a2abfec7334be0bdb9bc01eaf

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["rekee"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rekee/tile/struct.TileId.html\" title=\"struct rekee::tile::TileId\">TileId</a>","synthetic":false,"types":["rekee::tile::TileId"]}];
+implementors["rekee"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rekee/tile/struct.TileId.html\" title=\"struct rekee::tile::TileId\">TileId</a>","synthetic":false,"types":["rekee::tile::TileId"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
