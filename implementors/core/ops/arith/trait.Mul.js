1a258e0d7d13e0c6956d64853a16ee53854b3463
t-rapp <t-rapp@users.noreply.github.com>
2022-09-05 10:15:03 +0000

    deploy: 8a62539d8c644fc0a50af2f99094d739ee7a9f15

---
+++ implementors/core/ops/arith/trait.Mul.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["rekee"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.63.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.63.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"rekee/hexagon/struct.Point.html\" title=\"struct rekee::hexagon::Point\">Point</a>","synthetic":false,"types":["rekee::hexagon::Point"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var sourcesIndex = {};
-sourcesIndex["rekee"] = {"name":"","dirs":[{"name":"view","files":["catalog.rs","catalog_config.rs","export.rs","map.rs","map_config.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]}],"files":["controller.rs","edition.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs","token.rs"]};
+sourcesIndex["rekee"] = {"name":"","dirs":[{"name":"view","dirs":[{"name":"map","files":["config.rs","detail.rs","mod.rs"]}],"files":["catalog.rs","catalog_config.rs","export.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]}],"files":["controller.rs","edition.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs","token.rs"]};
 createSourceSidebar();
