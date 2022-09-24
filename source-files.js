82b872386ef1adadedaea20bcc88b28963a87576
t-rapp <t-rapp@users.noreply.github.com>
2022-09-24 21:51:12 +0000

    deploy: 9e85dbcea469d805f38b2a38dc39b351d0b09dba

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["rekee"] = {"name":"","dirs":[{"name":"view","dirs":[{"name":"map","files":["config.rs","detail.rs","mod.rs"]}],"files":["catalog.rs","catalog_config.rs","export.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]}],"files":["controller.rs","edition.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs","token.rs"]};
+var sourcesIndex = JSON.parse('{\
+"rekee":["",[["view",[["map",[],["config.rs","detail.rs","mod.rs"]]],["catalog.rs","catalog_config.rs","export.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]]],["controller.rs","edition.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs","token.rs"]]\
+}');
 createSourceSidebar();
