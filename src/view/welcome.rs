899f20aa6058bc198ad1ffbda463c4fe51525c48
Tobias Rapp <yahuxo+github@mailbox.org>
2022-12-11 08:38:05 +0100

    Add catalog tile series selection buttons to the welcome banner
    Makes it easier for users to find out how to create stages for DIRT.
    Leaving out the "all tiles" button on purpose as I guess that new users
    will likely be confused by the amount of tiles and the two competing
    filter button lines (lane width and terrain type). Power users will find
    out how to trigger the catalog configuration dialog.

--- src/view/welcome.rs
+++ src/view/welcome.rs
@@ -11,8 +11,10 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::{self, Element};
 
+use crate::check;
 use crate::controller::*;
 use crate::controller::welcome::*;
+use crate::edition::{Edition, Series};
 use super::*;
 
 //----------------------------------------------------------------------------
@@ -28,26 +30,64 @@ pub struct WelcomeSettings {
 pub struct WelcomeView {
     inner: Element,
     header: Element,
-    click_cb: Closure<dyn Fn(web_sys::Event)>,
+    close_cb: Closure<dyn Fn(web_sys::Event)>,
+    catalog_gt_button: Option<Element>,
+    catalog_dirt_button: Option<Element>,
+    catalog_all_button: Option<Element>,
+    select_catalog_cb: Closure<dyn Fn(web_sys::Event)>,
 }
 
 impl WelcomeView {
     pub fn new(parent: Element) -> Result<Self> {
+        let document = parent.owner_document().unwrap();
         let inner = parent;
 
         let header = inner.query_selector(".message-header")?
             .ok_or("Cannot find header of welcome message element")?;
         inner.set_hidden(false);
 
-        let click_cb = Closure::wrap(Box::new(move |_event: web_sys::Event| {
+        let close_cb = Closure::wrap(Box::new(move |_event: web_sys::Event| {
             nuts::publish(HideWelcomeEvent);
         }) as Box<dyn Fn(_)>);
 
         header.add_event_listener_with_callback("click",
-            click_cb.as_ref().unchecked_ref())?;
+            close_cb.as_ref().unchecked_ref())?;
         header.class_list().add_1("is-clickable")?;
 
-        Ok(WelcomeView { inner, header, click_cb })
+        let select_catalog_cb = Closure::wrap(Box::new(move |event: web_sys::Event| {
+            let button = check!(event.current_target()
+                .and_then(|target| target.dyn_into::<web_sys::HtmlElement>().ok()));
+            let series: Option<Series> = button.get_attribute("data-value")
+                .and_then(|val| val.parse().ok());
+            let edition_iter = if let Some(series) = series {
+                series.editions()
+            } else {
+                Edition::iter()
+            };
+            let editions: Vec<Edition> = edition_iter.copied()
+                    .collect();
+            nuts::publish(UpdateCatalogEditionsEvent { editions });
+        }) as Box<dyn Fn(_)>);
+
+        let catalog_gt_button = document.get_element_by_id("select-catalog-gt-button");
+        if let Some(ref button) = catalog_gt_button {
+            button.add_event_listener_with_callback("click", select_catalog_cb.as_ref().unchecked_ref())?;
+        }
+
+        let catalog_dirt_button = document.get_element_by_id("select-catalog-dirt-button");
+        if let Some(ref button) = catalog_dirt_button {
+            button.add_event_listener_with_callback("click", select_catalog_cb.as_ref().unchecked_ref())?;
+        }
+
+        let catalog_all_button = document.get_element_by_id("select-catalog-all-button");
+        if let Some(ref button) = catalog_all_button {
+            button.add_event_listener_with_callback("click", select_catalog_cb.as_ref().unchecked_ref())?;
+        }
+
+        Ok(WelcomeView {
+            inner, header, close_cb, catalog_gt_button, catalog_dirt_button,
+            catalog_all_button, select_catalog_cb
+        })
     }
 
     pub fn load_settings(&mut self, settings: &WelcomeSettings) {
@@ -71,7 +111,19 @@ impl WelcomeView {
 impl Drop for WelcomeView {
     fn drop(&mut self) {
         let _ = self.header.remove_event_listener_with_callback("click",
-            self.click_cb.as_ref().unchecked_ref());
+            self.close_cb.as_ref().unchecked_ref());
+        if let Some(ref button) = self.catalog_gt_button {
+            let _ = button.remove_event_listener_with_callback("click",
+                self.select_catalog_cb.as_ref().unchecked_ref());
+        }
+        if let Some(ref button) = self.catalog_dirt_button {
+            let _ = button.remove_event_listener_with_callback("click",
+                self.select_catalog_cb.as_ref().unchecked_ref());
+        }
+        if let Some(ref button) = self.catalog_all_button {
+            let _ = button.remove_event_listener_with_callback("click",
+                self.select_catalog_cb.as_ref().unchecked_ref());
+        }
     }
 }
 
