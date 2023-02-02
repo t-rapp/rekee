88085f6b4c3430cb310b7bcf2b71872bf875861d
t-rapp <t-rapp@users.noreply.github.com>
2023-02-02 13:09:22 +0000

    deploy: e5aae0a8501249d21e1f0e0d2169d4e1b87696f3

--- implementors/core/ops/arith/trait.Mul.js
+++ implementors/core/ops/arith/trait.Mul.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rekee":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.1/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"rekee/hexagon/struct.Point.html\" title=\"struct rekee::hexagon::Point\">Point</a>"]]
+"rekee":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"rekee/hexagon/struct.Point.html\" title=\"struct rekee::hexagon::Point\">Point</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/core/ops/index/trait.Index.js
+++ implementors/core/ops/index/trait.Index.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rekee":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"enum\" href=\"rekee/hexagon/enum.Direction.html\" title=\"enum rekee::hexagon::Direction\">Direction</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.1/std/primitive.array.html\">[T; 6]</a>"]]
+"rekee":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"enum\" href=\"rekee/hexagon/enum.Direction.html\" title=\"enum rekee::hexagon::Direction\">Direction</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.67.0/std/primitive.array.html\">[T; 6]</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
