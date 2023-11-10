cc42945b82dd67bd3169d2b776ce298d12fe657b
t-rapp <t-rapp@users.noreply.github.com>
2023-11-10 21:32:06 +0000

    deploy: e7a97c914fa97101fcc3c3184b0ee1db83d0c022

--- implementors/core/ops/arith/trait.Mul.js
+++ implementors/core/ops/arith/trait.Mul.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rekee":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"rekee/hexagon/struct.Point.html\" title=\"struct rekee::hexagon::Point\">Point</a>"]]
+"rekee":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.73.0/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.73.0/std/primitive.f32.html\">f32</a>&gt; for <a class=\"struct\" href=\"rekee/hexagon/struct.Point.html\" title=\"struct rekee::hexagon::Point\">Point</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
