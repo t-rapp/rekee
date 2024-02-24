03bbdcf599419e9a60a12b6407f704db8d75d8d1
t-rapp <t-rapp@users.noreply.github.com>
2024-02-24 20:25:05 +0000

    deploy: 4aa211eba99404c993fcdd7fb2d4790f39d0f712

--- implementors/core/hash/trait.Hash.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"rekee":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"rekee/tile/struct.TileId.html\" title=\"struct rekee::tile::TileId\">TileId</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"rekee/edition/enum.Series.html\" title=\"enum rekee::edition::Series\">Series</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"rekee":["",[["view",[["map",[],["config.rs","detail.rs","mod.rs","polar.rs"]]],["catalog.rs","catalog_config.rs","export.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]]],["controller.rs","edition.rs","export.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs","token.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["rekee",["",[["view",[["map",[],["config.rs","detail.rs","mod.rs","polar.rs"]]],["catalog.rs","catalog_config.rs","export.rs","mod.rs","track_info.rs","version.rs","welcome.rs"]]],["controller.rs","edition.rs","export.rs","hexagon.rs","import.rs","lib.rs","logger.rs","map.rs","storage.rs","tile.rs","token.rs"]]]\
+]'));
 createSrcSidebar();
