t-rapp/rekee

commit ffc4a1e129eb319720df3c5feeff85dccc1a5bb6

Cleanup mouse events on map view
Make buttons for the selected tile being activated only on a full mouse
click. Start and stop the dragging of map tiles only when the main
(left) mouse button was used.

Reduces conflict with browser built-in actions like panning.

src/view/map/mod.rs
@@ -239,7 +239,7 @@ impl SelectedMenu {
                 event.stop_propagation();
                 nuts::publish(RotateSelectedTileLeftEvent);
             }) as Box<dyn Fn(_)>);
-            btn.add_event_listener_with_callback("mousedown", callback.as_ref().unchecked_ref()).unwrap();
+            btn.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref()).unwrap();
             callback.forget();
         }
 
@@ -249,7 +249,7 @@ impl SelectedMenu {
                 event.stop_propagation();
                 nuts::publish(RotateSelectedTileRightEvent);
             }) as Box<dyn Fn(_)>);
-            btn.add_event_listener_with_callback("mousedown", callback.as_ref().unchecked_ref()).unwrap();
+            btn.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref()).unwrap();
             callback.forget();
         }
 
@@ -260,7 +260,7 @@ impl SelectedMenu {
                 nuts::send_to::<MapController, _>(SaveSettingsEvent);
                 nuts::publish(ShowMapDetailEvent);
             }) as Box<dyn Fn(_)>);
-            btn.add_event_listener_with_callback("mousedown", callback.as_ref().unchecked_ref()).unwrap();
+            btn.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref()).unwrap();
             callback.forget();
         }
 
@@ -270,7 +270,7 @@ impl SelectedMenu {
                 event.stop_propagation();
                 nuts::publish(RemoveSelectedTileEvent);
             }) as Box<dyn Fn(_)>);
-            btn.add_event_listener_with_callback("mousedown", callback.as_ref().unchecked_ref()).unwrap();
+            btn.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref()).unwrap();
             callback.forget();
         }
 
@@ -567,6 +567,9 @@ impl MapView {
 
         // add drag-n-drop event handlers to canvas element
         let callback = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if event.button() != 0 {
+                return; // only activate on the main (left) mouse button
+            }
             let pos = check!(mouse_position(&event));
             nuts::publish(UpdateSelectedTileEvent { pos });
             nuts::send_to::<MapController, _>(DragMapTileBeginEvent { pos });
@@ -581,6 +584,9 @@ impl MapView {
         }) as Box<dyn Fn(_)>);
 
         let dragged_mouseup_cb = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            if event.button() != 0 {
+                return; // only activate on the main (left) mouse button
+            }
             let pos = check!(mouse_position(&event));
             nuts::send_to::<MapController, _>(DragMapTileEndEvent { pos });
             nuts::publish(UpdateSelectedTileEvent { pos });

commit 93d85ca71d4a9238d0d05d77f4f4050d3e2acbcd

Fix broken click handlers of the selected tile menu
Separate the map tile drag-n-drop mousedown event, from updating the
selected tile with a click. Avoid updating the selected tile at the
start of a drag operation, and perform it at drag end only if the
position has changed.

Fixes a regression caused by ffc4a1e129eb319720df3c5feeff85dccc1a5bb6.

src/view/map/mod.rs
@@ -566,13 +566,26 @@ impl MapView {
         document.add_event_listener_with_callback("keyup",
             keychange_cb.as_ref().unchecked_ref())?;
 
+        // add tile-select event handler to canvas element
+        let callback = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+            let pos = check!(mouse_position(&event));
+            nuts::publish(UpdateSelectedTileEvent { pos });
+        }) as Box<dyn Fn(_)>);
+        canvas.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref()).unwrap();
+        callback.forget();
+
         // add drag-n-drop event handlers to canvas element
         let callback = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             if event.button() != 0 {
                 return; // only activate on the main (left) mouse button
             }
+            let maybe_button_target = check!(event.target()
+                .and_then(|target| target.dyn_into::<web_sys::Element>().ok())
+                .and_then(|target| target.closest("button,input[type=button]").ok()));
+            if maybe_button_target.is_some() {
+                return; // skip drag-n-drop when a selected-menu button is the event target
+            }
             let pos = check!(mouse_position(&event));
-            nuts::publish(UpdateSelectedTileEvent { pos });
             nuts::send_to::<MapController, _>(DragMapTileBeginEvent { pos });
         }) as Box<dyn Fn(_)>);
         canvas.add_event_listener_with_callback("mousedown", callback.as_ref().unchecked_ref()).unwrap();
@@ -590,7 +603,6 @@ impl MapView {
             }
             let pos = check!(mouse_position(&event));
             nuts::send_to::<MapController, _>(DragMapTileEndEvent { pos });
-            nuts::publish(UpdateSelectedTileEvent { pos });
         }) as Box<dyn Fn(_)>);
 
         let dragged_mouseleave_cb = Closure::wrap(Box::new(move |_event: web_sys::MouseEvent| {
@@ -1052,9 +1064,6 @@ impl MapView {
     pub fn drag_tile_begin(&mut self, pos: Point) {
         let pos = Coordinate::from_pixel_rounded(&self.layout,
             self.canvas_viewbox.top_left() + pos);
-        if self.selected.pos() != Some(pos) {
-            return;
-        }
         if let Some(tile) = self.map.get(pos) {
             info!("drag tile begin: {:?}", tile);
             let document = check!(self.canvas.owner_document());
@@ -1100,6 +1109,7 @@ impl MapView {
             if pos != tile.pos {
                 self.map.remove(tile.pos);
                 self.map.insert_with_tokens(tile.id(), pos, tile.dir, tile.tokens.clone());
+                self.inner_update_selected_tile(pos);
                 self.update_map();
             }
         }
@@ -1110,6 +1120,7 @@ impl MapView {
                 self.canvas_viewbox.top_left() + pos);
             info!("drag tile end: {:?} -> {:?}", tile, pos);
             self.map.append(tile, Some(pos), None);
+            self.inner_update_selected_tile(pos);
             self.update_map();
         }
 